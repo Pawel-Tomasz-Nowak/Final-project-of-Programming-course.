d03f908ad0dfc7beb0b54135ffaa541fecdffced
Pawel <pawelnowak2004priv@gmail.com>
2024-06-03 20:33:47 +0200

    Utworzenie pliku ze stałymi.

---
+++ Stałe.py
@@ -0,0 +1,13 @@
+from typing import Tuple
+
+# Kolory
+Color = Tuple[int, int, int]
+WHITE: Color = (255, 255, 255)
+MEDIUM_GRAY: Color = (100, 100, 100)
+ORANGE: Color = (255, 131, 32)
+BLACK: Color = (0, 0, 0)
+
+# Zadeklaruj szerokość i wysokość okna.
+screen_width: int = 400
+screen_height: int = 750
+window_size: tuple[int, int] = (screen_width, screen_height)
